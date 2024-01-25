8226713a47795c9d007088b374db8fd7c270ed87
mihir-robotics <mihir.kulkarni.808@gmail.com>
2024-01-25 19:15:10 +0530

    Unit Tests, Github Actions Test

---
+++ tests/flask_test.py
@@ -0,0 +1,32 @@
+# Import necessary modules
+import unittest
+import json
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
+
+class FlaskTest(unittest.TestCase):
+
+    # Ensure that Flask was set up correctly
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    # Ensure that the get-data page is working correctly
+    def test_get_data(self):
+        tester = app.test_client(self)
+        response = tester.get('/get-data', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    # Ensure that the send-data page is working correctly
+    def test_send_data(self):
+        tester = app.test_client(self)
+        response = tester.post('/send-data', data=json.dumps(dict(sensor_data='data')), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
